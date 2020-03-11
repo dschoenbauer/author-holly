9d1bcf94b9aabb19ad3dbd6df31bd70842067a7b
David Schoenbauer <d.schoenbauer@ctimeetingtech.com>
2020-03-10 21:40:20 -0500

    issue #6 Add Google Analytics

--- src/pages/BookLayout.js
+++ src/pages/BookLayout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Navigation } from "../components/Navigation";
 import { Title } from "../components/Title";
+import ReactGA from 'react-ga';
 
 export class BookLayout extends React.Component {
   render() {
+    ReactGA.pageview(window.location.pathname + window.location.search);
     return <div className='core-layout'>
       <Title></Title>
       <Navigation></Navigation>

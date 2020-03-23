b73d3efd63e3a9ae37b3d83848c329d117fed466
David Schoenbauer <d.schoenbauer@ctimeetingtech.com>
2020-03-23 15:14:51 -0500

    update all locations to local layout

---
+++ src/data/State.js
@@ -0,0 +1,35 @@
+export const State = {
+    books: [
+        { title: "", link: "", description: "", thumbnail: "", img: "", review: "" },
+    ],
+    contacts: [
+        {
+            "href": "tel:952-393-9558",
+            "desc": "Telephone: 952-393-9558",
+            "src": "./img/phone.png"
+        },
+        {
+            "href": "mailto:holly.schoenbauer@gmail.com",
+            "desc": "Email: holly.schoenbauer@gmail.com",
+            "src": "./img/email.png"
+        },
+        {
+            "href": "https://www.facebook.com/Holly-Schoenbauer-Childrens-Author-111720280401887/",
+            "desc": "Follow Me On Facebook",
+            "src": "./img/facebook.png"
+        },
+        {
+            "href": "https://twitter.com/H_Schoenbauer",
+            "desc": "Follow Me On Twitter",
+            "src": "./img/twitter.png"
+        },
+        {
+            "href": "https://www.instagram.com/hollyschoenbauer/",
+            "desc": "Follow Me On Instagram",
+            "src": "./img/instagram.png"
+        }
+    ],
+    blog: {
+        rssUrl: 'https://todaywearegoingto.blogspot.com/feeds/posts/default'
+    }
+};
